d920718ce74d334300e7763e49871d70e20e9ff6
yash <yashbhole6@gmail.com>
2024-04-01 01:12:54 +0530

    Improved the searching functionality

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const todoList = document.getElementById("super-hero-main");
 const addTaskForm = document.getElementById("super-hero-search");
-const newTaskInput = document.getElementById("hero-search");
-const addTaskBtn = document.getElementById("hero-search-btn");
+const heroSearch = document.getElementById("hero-search");
+const heroSearchButton = document.getElementById("hero-search-btn");
+const container = document.getElementById("results");
 
-const newTaskText = newTaskInput.value.trim();
-theUrl = `https://gateway.marvel.com:443/v1/public/characters?apikey=6ad77fac798bfe0a9c8599316689f1e6&hash=37a360b04ff2f9c78bd5eefe585dcdea&ts=1711821478916&limit=10`;
+theUrl = `https://gateway.marvel.com:443/v1/public/characters?apikey=6ad77fac798bfe0a9c8599316689f1e6&hash=37a360b04ff2f9c78bd5eefe585dcdea&ts=1711821478916&limit=50`;
 
 fetch(theUrl)
   .then((response) => response.json())
@@ -18,7 +18,7 @@ fetch(theUrl)
 
       const container = $("#results");
       const templateString = `
-          <div class="card mb-3" style="jumbotron-fluid">
+          <div class="card mb-3" style="jumbotron-fluid" id="Heros">
   <div class="row no-gutters">
     <div class="col-md-4">
       <img src=${image}.jpg class="card-img" alt="place holder for ${hero} image">
@@ -39,45 +39,21 @@ fetch(theUrl)
     // Handle the data here
   });
 
-addTaskBtn
-  .addEventListener("click", function () {
-    const newTaskText = newTaskInput.value.trim();
-    theUrl = `https://gateway.marvel.com:443/v1/public/characters?apikey=6ad77fac798bfe0a9c8599316689f1e6&hash=37a360b04ff2f9c78bd5eefe585dcdea&ts=1711821478916&limit=10`;
+heroSearchButton.addEventListener("click", function () {
+  var heroSearchInst = heroSearch.value;
+  console.log(heroSearchInst);
+  const element = document.getElementById("Heros");
+  do {
+    const element = document.getElementById("Heros");
+    // console.log(element);
+    element.remove();
+  } while (element);
+  var heroSearchInst = heroSearch.value.trim;
+  console.log(heroSearchInst);
+  // Handle any errors
+});
 
-    fetch(theUrl)
-      .then((response) => response.json())
-      .then((data) => {
-        op = data.data.results;
-        console.log(op);
-        for (var i = 0; i < op.length; i++) {
-          const hero = op[i].name;
-          const description = op[i].description;
-          const image = op[i].thumbnail.path;
-
-          const container = $("#results");
-          const templateString = `
-          <div class="card mb-3" style="jumbotron-fluid">
-  <div class="row no-gutters">
-    <div class="col-md-4">
-      <img src=${image}.jpg class="card-img" alt="...">
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">${hero}</h5>
-        <p class="card-text">${description}</p>
-        <a href="#" class="btn btn-primary">Go Somewhere</a>
-      </div>
-    </div>
-  </div>
-</div>
-         
-      `;
-          container.append(templateString);
-        }
-        // Handle the data here
-      });
-  })
-  .catch((error) => {
-    console.log("Error=> " + error);
-    // Handle any errors
-  });
+// heroSearchButton.addEventListener("click", function () {
+//   var heroSearchInst = heroSearch.value.trim;
+//   console.log(heroSearchInst);
+// });
