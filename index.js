73cccbacc7572b00e9215508a38c02142516b38d
Yash Bhole <51267869+bhole-yash@users.noreply.github.com>
2024-04-05 04:23:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const heroSearch = document.getElementById("hero-search");
 const heroSearchButton = document.getElementById("hero-search-btn");
 const container = document.getElementById("results");
 
-theUrl = `https://gateway.marvel.com:443/v1/public/characters?apikey=6ad77fac798bfe0a9c8599316689f1e6&hash=37a360b04ff2f9c78bd5eefe585dcdea&ts=1711821478916&limit=50`;
+var theUrl = `https://gateway.marvel.com:443/v1/public/characters?apikey=6ad77fac798bfe0a9c8599316689f1e6&hash=37a360b04ff2f9c78bd5eefe585dcdea&ts=1711821478916&limit=50`;
 
 fetch(theUrl)
   .then((response) => response.json())
@@ -40,18 +40,63 @@ fetch(theUrl)
   });
 
 heroSearchButton.addEventListener("click", function () {
-  var heroSearchInst = heroSearch.value;
-  console.log(heroSearchInst);
-  const element = document.getElementById("Heros");
-  do {
-    const element = document.getElementById("Heros");
-    // console.log(element);
-    element.remove();
-  } while (element);
-  var heroSearchInst = heroSearch.value.trim;
-  console.log(heroSearchInst);
-  // Handle any errors
+  var heroSearchInst = heroSearch.value
+  console.log(typeof(heroSearchInst),0);
+  emptyResults()
+  console.log(heroSearchInst,1);
+  var searchString = theUrl +"&nameStartsWith="+heroSearchInst;
+  console.log(searchString)
+
+  fetch(searchString)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data)
+    op = data.data.results;
+    console.log(op);
+    for (var i = 0; i < op.length; i++) {
+      const hero = op[i].name;
+      const description = op[i].description;
+      const image = op[i].thumbnail.path;
+
+      const container = $("#results");
+      const templateString = `
+          <div class="card mb-3" style="jumbotron-fluid" id="Heros">
+  <div class="row no-gutters">
+    <div class="col-md-4">
+      <img src=${image}.jpg class="card-img" alt="place holder for ${hero} image">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">${hero}</h5>
+        <p class="card-text">${description}</p>
+        <a href="#" class="btn btn-primary">Go Somewhere</a>
+      </div>
+    </div>
+  </div>
+</div>
+         
+      `;
+      container.append(templateString);
+    }
+    // Handle the data here
+  });
+   
 });
+
+function emptyResults(){
+  try{
+    const element = document.getElementById("Heros");
+    do {
+      const element = document.getElementById("Heros");
+      // console.log(element);
+      element.remove();
+    } while (element);
+  }
+  catch(err) {
+    console.log(err)
+  } 
+  
+}
 
 // heroSearchButton.addEventListener("click", function () {
 //   var heroSearchInst = heroSearch.value.trim;
