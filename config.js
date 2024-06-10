6deaac7abb4f98fdee4b6089ac7662f43d594b20
Bhole, Yash (Infosys Limited) <yash.bhole@astrazeneca.com~>
2024-06-10 17:53:31 +0530

    taking a risk

---
+++ config.js
@@ -0,0 +1,5 @@
+export const apiKey = '6ad77fac798bfe0a9c8599316689f1e6';
+export const hash = '37a360b04ff2f9c78bd5eefe585dcdea';
+export const ts = 1711821478916; // You can update this timestamp
+
+export const baseUrl = `https://gateway.marvel.com:443/v1/public/characters`;
